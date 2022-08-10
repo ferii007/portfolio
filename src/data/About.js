dd4206cb4d477f32e17c45fba003a1d0f9325f97
Feri Ramdani <ferii.ramdani007@gmail.com>
2022-08-10 15:20:00 +0700

    update about section

---
+++ src/data/About.js
@@ -0,0 +1,135 @@
+
+import htmlIcon from './../assets/img/icon/html-icon.svg';
+import cssIcon from './../assets/img/icon/css-icon.svg';
+import jsIcon from './../assets/img/icon/js-icon.svg';
+import phpIcon from './../assets/img/icon/php-icon.svg';
+import reactIcon from './../assets/img/icon/react-icon.svg';
+import bootstrapIcon from './../assets/img/icon/bootstrap-icon.svg';
+import tailwindIcon from './../assets/img/icon/tailwind-icon.svg';
+import laravelIcon from './../assets/img/icon/laravel-icon.svg';
+import cypressIcon from './../assets/img/icon/cypress-icon.svg';
+import postmanIcon from './../assets/img/icon/postman-icon.svg';
+import gitIcon from './../assets/img/icon/git-icon.svg';
+
+export const listSkills = [
+    {
+        "name": "Primary Skills"
+    },
+
+    {
+        "name": "Other Skills"
+    }
+]
+
+export const myPrimarySkills = [
+    {
+        name: "HTML",
+        icon: htmlIcon,
+        level: "Expert",
+        bgColor: '#ef652a'
+    },
+
+    {
+        name: "CSS",
+        icon: cssIcon,
+        level: "Expert",
+        bgColor: '#1572b6'
+    },
+
+    {
+        name: "JS",
+        icon: jsIcon,
+        level: "Intermediate",
+        bgColor: '#f7df1e'
+    },
+
+    {
+        name: "PHP",
+        icon: phpIcon,
+        level: "Intermediate",
+        bgColor: '#6181B6'
+    },
+
+    {
+        name: "React",
+        icon: reactIcon,
+        level: "Beginner",
+        bgColor: '#333'
+    },
+
+    {
+        name: "Bootstrap",
+        icon: bootstrapIcon,
+        level: "Expert",
+        bgColor: '#563D7C'
+    },
+
+    {
+        name: "Tailwind",
+        icon: tailwindIcon,
+        level: "Expert",
+        bgColor: '#f7f0f0'
+    },
+
+    {
+        name: "Laravel",
+        icon: laravelIcon,
+        level: "Beginner",
+        bgColor: '#f7f0f0'
+    },
+];
+
+export const myOtherSkills = [
+    {
+        name: "Cypress",
+        icon: cypressIcon,
+        level: "Intermediate",
+        bgColor: '#ef652a'
+    },
+
+    {
+        name: "Postman",
+        icon: postmanIcon,
+        level: "Intermediate",
+        bgColor: '#ef652a'
+    },
+
+    {
+        name: "Git",
+        icon: gitIcon,
+        level: "Intermediate",
+        bgColor: '#ef652a'
+    },
+]
+
+export const myBiodata = [
+    {
+        key: 'Place of Birth',
+        value: 'Bandung'
+    },
+
+    {
+        key: 'Date of Birth',
+        value: 'Feb 02 2003'
+    },
+
+    {
+        key: 'Gender',
+        value: 'Male'
+    },
+
+    {
+        key: 'Nationally',
+        value: 'Indonesia'
+    },
+
+    {
+        key: 'Religion',
+        value: 'Islam'
+    },
+
+    {
+        key: 'Address',
+        value: 'Bandung, Kopo Street'
+    },
+];
