bb86f1afc78908f80df50a8b544e226c0f40b188
Feri Ramdani <ferii.ramdani007@gmail.com>
2022-09-23 18:03:05 +0700

    coba testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = app => {
     app.use(
-        createProxyMiddleware('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Html',{
-            target: 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Html',
+        createProxyMiddleware('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1',{
+            target: 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1',
             changeOrigin: true
         })
     )
